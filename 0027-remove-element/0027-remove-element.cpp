fe980070837f82c1912dcad4d18a4fb29dcee49e
imani1511 <111653863+imani1511@users.noreply.github.com>
2023-01-21 00:45:53 +0530

    Time: 3 ms (63.69%), Space: 8.9 MB (42.51%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int>::iterator it= nums.begin();
+        while(it != nums.end()){
+            if(*it == val){
+                it= nums.erase(it);
+            }
+            else
+            it++;
+        }
+        int len = nums.size();
+        return len;
+
+    }
+};
