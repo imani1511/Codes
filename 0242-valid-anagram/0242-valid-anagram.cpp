1e75a6953f1dfd4ac58c77eaf7ca93cfcb13b4d6
imani1511 <111653863+imani1511@users.noreply.github.com>
2023-12-18 13:06:21 +0530

    Time: 7 ms (74.03%), Space: 7.6 MB (77.41%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        
+        if(n != m) return false;
+        
+        int map[26] ={0};
+        
+        for(int i=0;i<n;i++){
+            map[s[i] - 'a']++;
+            map[t[i] - 'a']--;
+        }
+        
+        for(int i : map){
+            if(i != 0) return false;
+        }
+        return true;
+    }
+};
